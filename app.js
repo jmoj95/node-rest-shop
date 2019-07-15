b9ca5affbd7b2f0aaf8bd39b3f5205a33fc93e32
jmoj95 <jmoj95@jmoj95.com>
2019-07-15 09:48:23 -0300

    Part 04 of the tutorial done.

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
+
+app.use(morgan('dev'))
 
 const productRoutes = require('./api/routes/products')
 app.use('/products', productRoutes)
 
 const orderRoutes = require('./api/routes/orders')
 app.use('/orders', orderRoutes)
+
+app.use((req, res, next) => {
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({
+    error: {
+      status,
+      message: err.message
+    }
+  })
+})
 
 module.exports = app
