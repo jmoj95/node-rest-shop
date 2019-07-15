ea25d775230d77c9d47b1b97e7f027f035c2cd95
jmoj95 <jmoj95@jmoj95.com>
2019-07-15 09:20:19 -0300

    Part 03 of the tutorial done.

---
+++ api/routes/products.js
@@ -0,0 +1,44 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /products'
+  })
+})
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id
+  if (id === 'special') {
+    res.status(200).json({
+      message: 'You discovered the special ID',
+      id
+    })
+  } else {
+    res.status(200).json({
+      message: 'You passed an ID',
+      id
+    })
+  }
+})
+
+router.post('/', (req, res, next) => {
+  res.status(201).json({
+    message: 'Handling POST requests to /products'
+  })
+})
+
+router.patch('/:id', (req, res, next) => {
+  res.status(200).json({
+    message: 'Updated product.'
+  })
+})
+
+router.delete('/:id', (req, res, next) => {
+  res.status(200).json({
+    message: 'Deleted product.'
+  })
+})
+
+module.exports = router
