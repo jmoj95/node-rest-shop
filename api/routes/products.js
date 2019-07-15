de4d281e3fcffb4627d95ef518be36861a3753cd
jmoj95 <jmoj95@jmoj95.com>
2019-07-15 10:10:56 -0300

    Part 05 of the tutorial done.

--- api/routes/products.js
+++ api/routes/products.js
@@ -24,8 +24,13 @@ router.get('/:id', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  }
   res.status(201).json({
-    message: 'Handling POST requests to /products'
+    message: 'Handling POST requests to /products',
+    product
   })
 })
 
--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  if (req.method === 'OPTIONS') {
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, PATCH, DELETE'
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 
 const productRoutes = require('./api/routes/products')
 app.use('/products', productRoutes)
